956ddb4a27e1dd40e58b8831b0ce0a0afe9f3796
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-10-23 20:51:54 -0700

    IR5

--- ZombieGame.java
+++ ZombieGame.java
@@ -3,12 +3,5 @@ class ZombieGame {
         Map map = new Map();
         Person player = new Person();
         map.setGrid(10);
-        map.getGrid();
-
-        player.moveDown();
-        player.moveForward();
-        map.setPlayerPos(player.xpos, player.ypos);
-        System.out.println("");
-        map.getGrid();
     }
 }
